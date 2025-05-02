330308d055fe84d03070ddc32a43bab3104eb63a
AchiugoTrust <achiugotrust@gmail.com>
2025-05-02 06:01:32 +0100

    code break -1

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -17,7 +17,7 @@ func TestGetAPIKey(t *testing.T) {
 		{
 			name:         "valid header",
 			header:       http.Header{"Authorization": []string{"ApiKey abc123"}},
-			expectedKey:  "wrongkey",
+			expectedKey:  "wrongkey lol",
 			expectingErr: false,
 		},
 		{
